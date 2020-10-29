b0c951846f03160952fccbadc9a97ccb05c7a63f
Tobias A. Jayne <toby.jayne@gmail.com>
2020-10-29 13:43:50 -0500

    Created Data Classes and DTOs.  Also included AutoMapper and changed appsettings file.

---
+++ BookStore-API/DTOs/AuthorDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.DTOs
+{
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Bio { get; set; }
+        public virtual IList<BookDTO> Books { get; set; }
+    }
+}
---
+++ BookStore-API/Data/Author.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore_API.Data
+{
+    [Table("Authors")]
+    public partial class Author
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Bio { get; set; }
+        public virtual IList<Book> Books { get; set; }
+
+
+    }
+}
